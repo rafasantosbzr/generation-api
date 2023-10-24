6306686f8874c8bc9340ed4725a5670e0a64efdb
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-24 17:52:14 -0300

    api configurada com sucesso

--- src/dumpl.sql
+++ src/dumpl.sql
@@ -4,8 +4,8 @@ CREATE TABLE alunos (
     id_aluno serial PRIMARY KEY,
     nome TEXT NOT NULL,
     idade INT NOT NULL,
-    primeira_nota DECIMAL(3, 2) NOT NULL,
-    segunda_nota DECIMAL(3, 2) NOT NULL,
+    primeira_nota TEXT NOT NULL,
+    segunda_nota TEXT NOT NULL,
     professor_nome TEXT NOT NULL,
-    sala INT NOT NULL
+    sala TEXT NOT NULL
 );
