570d8939629969f743dd3cf447753791e79551a9
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-25 18:52:07 -0300

    especificação das rotas na página inicial da aplicação

--- src/rotas.js
+++ src/rotas.js
@@ -3,7 +3,37 @@ const { listarAlunos, cadastrarAluno, obterAluno, atualizarAluno, excluirAluno }
 const rotas = express();
 
 rotas.get('/', (req, res) => {
-    res.send('Página inicial do servidor.');
+    res.send(
+`
+Página inicial do servidor.
+
+GET /alunos
+//faz uma listagem de todos os alunos cadastrados no sistema
+
+POST /alunos
+//faz o cadastro de um novo aluno
+
+exemplo de request body json:
+{
+    "nome": "José",
+    "idade": "30",
+    "primeira_nota": "75,00",
+    "segunda_nota": "80,00",
+    "professor_nome": "Maria",
+    "sala": "A"
+}
+
+GET /alunos/:id
+//faz a busca de um aluno no sistema pelo ID do aluno
+
+PUT /alunos/:id
+//faz a atualização das informações de um aluno no sistema
+
+DELETE /alunos/:id
+//faz a exclusão do cadastro de um aluno no sistema pelo ID do aluno
+`
+);
+
 })
 //faz uma listagem de todos os alunos cadastrados no sistema
 rotas.get('/alunos', listarAlunos);
