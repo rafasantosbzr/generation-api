8598f869c75b73bbe8ac82e09d202d1c58cd9ba7
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-30 20:06:32 -0300

    config do swagger

--- src/swagger.js
+++ src/swagger.js
@@ -9,9 +9,7 @@ const doc = {
   };
   
   const outputFile = './swagger-output.json';
-  const routes = ['./src/rotas.js'];
+  const routes = ['./src/index.js', './src/rotas.js'];
   
-  /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-  root file where the route starts, such as index.js, app.js, routes.js, etc ... */
   
   swaggerAutogen(outputFile, routes, doc);
